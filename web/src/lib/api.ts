8f0b7b6dab8a44d3519c131dbd8334399a329c3d
Seabkird <32527679+Seabkird@users.noreply.github.com>
2026-01-14 10:59:47 +0100

    ajout env api url

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,7 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const fetchHello = async () => {
-  const res = await fetch('/api/user'); 
+  const res = await fetch('${apiUrl}/api/user'); 
   if (!res.ok) throw new Error('Erreur réseau');
   return res.json();
 };
